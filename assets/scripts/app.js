e31ed0b79091ae4accb73d12c87779f2233404bd
Arindam Bajpayee <ab4dev@gmail.com>
2017-10-24 11:49:02 +0200

    updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,68 +1,73 @@
-var app = angular.module('appDist',[]);
+var app = angular.module('appDist', []);
 
-app.controller('appDistController', function($scope, $http, $filter) {
-    var releasesURL = "https://api.github.com/repos/sonect/app-dist/releases"
+app.controller('appDistController', function ($scope, $http, $filter) {
+    var releasesURL = "https://api.github.com/repos/sonect/app-dist/releases";
 
     var links = document.getElementsByTagName('a');
     var releases = [];
 
-    for(var i = 0; i < links.length; i++) {
-      var link = links[i];
-      var title = link.parentNode.getElementsByClassName('card-title')[0];
+    for (var i = 0; i < links.length; i++) {
+        var link = links[ i ];
+        var title = link.parentNode.getElementsByClassName('card-title')[ 0 ];
 
-      var platforms = releases[title.textContent] || [];
+        var platforms = releases[ title.textContent ] || [];
 
-      if (link.href.indexOf("itms") != -1) {
-        platforms['iOS'] = link;
-        link.classList.add("disabled");
-      } else if (link.href.indexOf("apk") != -1) {
-        platforms['Android'] = link;
-        link.classList.add("disabled");
-      }
+        if (link.href.indexOf("itms") != -1) {
+            platforms[ 'iOS' ] = link;
+            link.classList.add("disabled");
 
-      releases[title.textContent] = platforms
+        } else if (link.href.indexOf("apk") != -1) {
+            platforms[ 'Android' ] = link;
+            link.classList.add("disabled");
+        }
+
+        releases[ title.textContent ] = platforms
     }
 
     if (Object.keys(releases).length) {
-      $http.get(releasesURL).
-      success(function(data, status, headers, config) {
+        $http.get(releasesURL).success(function (data, status, headers, config) {
 
-        for(var i = 0; i < data.length; i++) {
-          var release = data[i];
+            for (var i = 0; i < data.length; i++) {
+                var release = data[ i ];
 
-          var releasePlatforms  = releases[release.name];
+                var releasePlatforms = releases[ release.name ];
 
-          if (angular.isDefined(releasePlatforms)) {
+                if (angular.isDefined(releasePlatforms)) {
 
-            for(var j = 0; j < release.assets.length; j++) {
-              var asset = release.assets[j];
-              var releaseLink;
+                    for (var j = 0; j < release.assets.length; j++) {
+                        var asset = release.assets[ j ];
+                        var releaseLink;
 
-              if (asset.name.indexOf("ipa") != -1) { // iOS
-                releaseLink = releasePlatforms["iOS"];
-              } else if (asset.name.indexOf("apk") != -1) {
-                releaseLink = releasePlatforms["Android"];
-              }
+                        if (asset.name.indexOf("ipa") != -1) { // iOS
+                            releaseLink = releasePlatforms[ "iOS" ];
+                        } else if (asset.name.indexOf("apk") != -1) {
+                            releaseLink = releasePlatforms[ "Android" ];
+                        }
 
-              if (angular.isDefined(releaseLink)) {
-                releaseLink.classList.remove("disabled");
-                var ul = releaseLink.parentNode.getElementsByTagName('ul')[0];
-                if (angular.isDefined(ul)) {
-                  var li = document.createElement("li");
-                  var date = $filter('date')(new Date(asset.updated_at),'dd MMM hh:mm a');
-                  li.appendChild(document.createTextNode(asset.uploader["login"]+ " @ " + date ));
-                  ul.appendChild(li);
+                        if (angular.isDefined(releaseLink)) {
+                            releaseLink.classList.remove("disabled");
+                            var cardNode = releaseLink.parentNode;
+                            var cardTitleNode = cardNode.getElementsByClassName('card-title')[0];
+                            var cardSubTitleNode, date;
+
+                            if (angular.isDefined(cardTitleNode)) {
+                                cardSubTitleNode = document.createElement('h6');
+                                cardSubTitleNode.className = 'card-subtitle mb-2 text-muted';
+
+                                date = $filter('date')(new Date(asset.updated_at), 'dd.MM.yyyy (HH:mm:ss)');
+                                cardSubTitleNode.appendChild(document.createTextNode(date + " - " + asset.uploader[ "login" ]));
+
+                                cardNode.insertBefore(cardSubTitleNode, cardTitleNode.nextSibling);
+                            }
+                        }
+
+                    }
                 }
-              }
-
             }
-          }
-        }
 
-      }).
-      error(function(data, status, headers, config) {
-        // log error
-      });
+        }).error(function (data, status, headers, config) {
+            // log error
+        });
     }
 
-})
+});
